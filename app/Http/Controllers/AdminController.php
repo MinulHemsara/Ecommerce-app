73895df0c58237d1e54a7685b145b90ce586c96d
Minul Hemsara <codse211f033@student.nibm.lk>
2025-01-08 22:36:06 +0530

    admin change password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -57,5 +58,25 @@ class AdminController extends Controller
         $notification = ['message'=>'Admin profile updated successfully','alert-type'=>'success'];
         
         return redirect()->back()->with($notification);
+    }
+
+    public function AdminChangePassword(){
+        return view ('admin.admin_change_password');
+    }
+    
+    public function AdminUpdatepassword(Request $request){
+        $request->validate([
+            'old_password'=>'required',
+            'new_password' =>'required|confirmed',
+        ]);
+
+        if(!Hash::check($request->old_password, auth::user()->password)){
+            return back()->with('error', 'Old Password Does not Match!!');
+        }
+
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+        return back()->with('status', "Password Change Successfully");
     }
 }
