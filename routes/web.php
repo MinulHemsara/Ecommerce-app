23520c5d94c31851126f666b808cb30205d2be0a
Minul Hemsara <codse211f033@student.nibm.lk>
2025-01-12 23:50:01 +0530

    vendor panel setup

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,15 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 //Vendor
 Route::middleware(['auth', 'role:vendor'])->group(function () {
     Route::get('/vendor/dashboard', [VendorController::class, 'vendorDashboard'])->name('vendor.dashboard');
+    Route::get('/vendor/logout', [VendorController::class, 'VendorDestroy'])->name('vendor.logout');
+
+    Route::get('/vendor/profile', [VendorController::class, 'VendorProfile'])->name('vendor.profile');
+    Route::post('/vendor/profile/store', [VendorController::class, 'VendorProfileStore'])->name('vendor.profile.store');
+
+    Route::get('/vendor/change/password', [VendorController::class, 'VendorChangePassword'])->name('change.password');
+    Route::post('/vendor/update/password', [VendorController::class, 'VendorUpdatepassword'])->name('update.password');
+
 });
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
+Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
